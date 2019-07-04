7ac9b3d39a645b1fec6e05795b90197d7c77c5d1
mr-fool <mr-fool@users.noreply.github.com>
2019-07-03 18:17:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const axios = require('axios');
+// Send a POST request
+axios({
+    method: 'post',
+    url: 'https://mcdan-coding-exercise.azurewebsites.net/api/v1/admin/register',
+    data: {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    }
+  });
